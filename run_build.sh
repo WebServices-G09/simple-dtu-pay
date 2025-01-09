e253d83b89862cde60193731067481350ec6ec6b
Ionut Eusebiu Andrici <ionut.andrici2@mail.dcu.ie>
2025-01-09 10:38:13 +0100

    Update run_build.sh

--- run_build.sh
+++ run_build.sh
@@ -6,8 +6,8 @@ echo "Building the Maven project..."
 mvn -f ./API/pom.xml clean package
 
 echo "Building and running Docker containers..."
-docker-compose build
-docker-compose up -d
+docker compose build
+docker compose up -d
 
 echo "Displaying logs..."
-docker-compose logs -f
+docker compose logs -f
