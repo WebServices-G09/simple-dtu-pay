88ab605dabc773295f0a86e840b50f80761adacb
raihanmehran <raihan.mehran@hotmail.com>
2025-01-09 16:07:28 +0100

    API: deleteCustomer endpoint added

--- API/src/main/java/Controllers/CustomerController.java
+++ API/src/main/java/Controllers/CustomerController.java
@@ -74,4 +74,29 @@ public class CustomerController
         }
     }
 
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCustomer(@PathParam("id") UUID id) {
+        try {
+            boolean isDeleted = customerService.deleteCustomer(id);
+
+            if (!isDeleted) {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("{\"error\": \"Customer does not exist\"}")
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
+
+            return Response.status(Response.Status.OK)
+                    .entity("{\"message\": \"Customer deleted successfully\"}")
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("{\"error\": \"An unexpected error occurred\"}")
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+    }
 }
